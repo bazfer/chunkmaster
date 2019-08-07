eae3001005a59f676f02631506b32b8f6394ca68
Fernando Baz <bazfer@gmail.com>
2019-08-07 13:02:48 -0500

    re-add guards when no current user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @chunk_feed = current_user.chunk_feed
+    @chunk_feed = current_user.chunk_feed if current_user
     @chunk = Chunk.new
   end
 end
